f2a992c7640981adbdaf0a4c53a35b2996605ea9
OscarRanilla <oscarjosemanuel89@gmail.com>
2025-03-08 22:10:31 +0100

    Cambios realizados en App.jsx, CardEmployee.css, CardEmployee.jsx, top.css, top.jsx 08/03/2025

---
+++ src/CardEmployee.jsx
@@ -0,0 +1,32 @@
+import Header from './top'
+import './App.css'
+import Calculator from './Calculator';
+import Footer from './footer'
+import EmployeeList from './CardEmployee'
+
+function App() {
+  //ejemplo para cambiar luego patata por el resultado que tengo que guardar en las variables para que haga la suma. Puedo usar las mismas variables para el resto */
+    return (
+    // tengo que poner aquí el logo en lo de arriba. Que no se me olvide {almacenamiento}
+
+    <div className='main'>
+        <Header /> 
+        <div className="container">
+            <section className="section-cards">
+                <EmployeeList/>
+            </section>
+
+            <section className='section-forms'>
+                <Calculator />
+            </section>
+    
+        </div>
+        <Footer/>
+    </div>
+
+    
+    /* <pie /> */
+)
+}
+
+export default App
---
+++ src/top.jsx
@@ -0,0 +1,14 @@
+
+import logo from "./assets/cocretainc.jpg";
+
+const Top = () => {
+return (
+    <header className="header">
+        <img className="logo" src={logo} alt="cocretainc"   />
+        </header>
+
+
+    );
+};
+
+export default Top;
